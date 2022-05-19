c46338c3ab5b51a09fce52efa9d48b5f731e2fba
krkhnh <krkhnh@gmail.com>
2022-05-20 08:54:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/mykioskbackend/MyKioskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mykioskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKioskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKioskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mykioskbackend/MyKioskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mykioskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKioskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
