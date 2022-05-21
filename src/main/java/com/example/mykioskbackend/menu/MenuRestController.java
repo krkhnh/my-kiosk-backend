940aa809918a21983f3aa9c9dcc53169fac26369
krkhnh <krkhnh@gmail.com>
2022-05-21 12:47:31 +0900

    CORS 허용

--- src/main/java/com/example/mykioskbackend/menu/MenuRestController.java
+++ src/main/java/com/example/mykioskbackend/menu/MenuRestController.java
@@ -2,6 +2,7 @@ package com.example.mykioskbackend.menu;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/menus")
+@CrossOrigin
 @RequiredArgsConstructor
 public class MenuRestController {
 	@NonNull
